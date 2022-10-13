81a36c589bec7236b63ba5be4234269bf5e493de
wellsparksoon <108337267+wellsparksoon@users.noreply.github.com>
2022-10-13 15:12:46 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,45 @@
+# Goorm 搭建 xray vmess / vless 节点
+
+使用goorm的免费ide，配合其alwayson功能和端口转发，实现xray代理服务
+
+# 注：请勿在常用账号部署本项目
+
+## 食用方法
+
+博客图文教程：https://owo.misaka.rest/goormide-xray/
+
+1. 注册并登录Goorm账号
+2. 创建一个Container
+3. 输入容器名称、选择地区，Slack选择blank，点击创建按钮创建容器
+
+![](https://gcore.jsdelivr.net/gh/Misaka-blog/tuchuang@master/20220531052101.png)
+
+![](https://gcore.jsdelivr.net/gh/Misaka-blog/tuchuang@master/20220531052141.png)
+
+4. 输入以下命令
+
+```shell
+wget -N https://raw.githubusercontent.com/Misaka-blog/GX/master/misaka.sh && bash misaka.sh
+```
+
+5. 配置端口转发
+
+![](https://gcore.jsdelivr.net/gh/Misaka-blog/tuchuang@master/20220531052447.png)
+
+6. vmess / vless 配置如下
+
+```
+地址：IP
+端口：转发的端口
+默认UUID：8d4a8f5e-c2f7-4c1b-b8c0-f8f5a9b6c384
+vmess额外id：0
+加密：auto 或 none
+传输协议：ws
+伪装类型：none
+```
+
+IP被墙？关机，重启即可更换IP
+
+## 免责声明
+
+本项目仅为个人研究软件原理、效果之用，不对部署产生的任何后果负责
---
+++ misaka.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+
+RED="\033[31m"
+GREEN="\033[32m"
+YELLOW="\033[33m"
+PLAIN='\033[0m'
+
+red() {
+    echo -e "\033[31m\033[01m$1\033[0m"
+}
+
+green() {
+    echo -e "\033[32m\033[01m$1\033[0m"
+}
+
+yellow() {
+    echo -e "\033[33m\033[01m$1\033[0m"
+}
+
+clear
+echo "#############################################################"
+echo -e "#                 ${RED} Goorm Xray 一键安装脚本${PLAIN}                  #"
+echo -e "# ${GREEN}作者${PLAIN}: Misaka No                                           #"
+echo -e "# ${GREEN}网址${PLAIN}: https://owo.misaka.rest                             #"
+echo -e "# ${GREEN}论坛${PLAIN}: https://vpsgo.co                                    #"
+echo -e "# ${GREEN}TG群${PLAIN}: https://t.me/misakanetcn                            #"
+echo "#############################################################"
+echo ""
+
+yellow "使用前请注意："
+red "1. 我已知悉本项目有可能触发 Goorm 封号机制"
+red "2. 我不保证脚本其搭建节点的稳定性"
+read -rp "是否安装脚本？ [Y/N]：" yesno
+
+if [[ $yesno =~ "Y"|"y" ]]; then
+    rm -f railgun kazari.json
+    yellow "开始安装..."
+    wget -N https://raw.githubusercontent.com/Misaka-blog/GX/master/railgun
+    chmod +x railgun
+    read -rp "请设置UUID（如无设置则使用脚本默认的）：" uuid
+    if [[ -z $uuid ]]; then
+        uuid="8d4a8f5e-c2f7-4c1b-b8c0-f8f5a9b6c384"
+    fi
+    cat <<EOF > kazari.json
+{
+    "log": {
+        "loglevel": "warning"
+    },
+    "inbounds": [
+        {
+            "port": 11111,
+            "protocol": "vmess",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "$uuid"
+                    }
+                ],
+                "decryption": "none"
+            },
+            "streamSettings": {
+                "network": "ws",
+                "security": "none"
+            }
+        },
+        {
+            "port": 11111,
+            "protocol": "vless",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "$uuid"
+                    }
+                ],
+                "decryption": "none"
+            },
+            "streamSettings": {
+                "network": "ws",
+                "security": "none"
+            }
+        }
+    ],
+    "outbounds": [
+        {
+            "protocol": "freedom"
+        }
+    ]
+}
+EOF
+    nohup ./railgun -config=kazari.json &>/dev/null &
+    green "Goorm Xray 已安装完成！"
+    yellow "请认真阅读项目说明文档，配置端口转发！"
+    yellow "别忘记给项目点一个免费的Star！"
+else
+    red "已取消安装，脚本退出！"
+    exit 1
+fi
